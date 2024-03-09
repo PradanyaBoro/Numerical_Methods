6f87cfd66152d0510dd2adbd0a064b2b4b6930c7
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-09 11:45:35 +0530

    Update BisectionMethod.m

--- Methods of Solving Algebraic and Transcendental Equations/BisectionMethod.m
+++ Methods of Solving Algebraic and Transcendental Equations/BisectionMethod.m
@@ -1,21 +1,27 @@
-syms x 'real'
+syms x 'real';
 
 f = sin(x) + x^2 - 1;
 
+% Define the initial interval [a, b]
 a = 0;
 b = 1;
 
+% Initialize the midpoint of the interval
 x0 = (b + a) / 2;
 
+% Loop until the desired accuracy is achieved
 while abs(b - a) > (10^(-4))
+    % Evaluate the function at the midpoint
     f_val = subs(f, x, x0);
 
+    % Update the interval [a, b] based on the sign of f(x0)
     if f_val < 0
-        a = x0;
+        a = x0; % Update 'a' if f(x0) is negative
     elseif f_val > 0
-        b = x0;
+        b = x0; % Update 'b' if f(x0) is positive
     end
 
+    % Update the midpoint of the interval
     x0 = (b + a) / 2;
 end
 
