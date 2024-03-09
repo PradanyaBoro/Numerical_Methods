07835599ecdcebadbfc898e0ef6be6f0d5aae52f
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-09 11:39:01 +0530

    Update BisectionMethod.m

--- Methods of Solving Algebraic and Transcendental Equations/BisectionMethod.m
+++ Methods of Solving Algebraic and Transcendental Equations/BisectionMethod.m
@@ -1,28 +1,22 @@
-% Define the function f(x) to be solved using a function handle
-f = @(x) sin(x) + x.^2 - 1;
+syms x 'real'
 
-% Define the initial interval [a, b]
+f = sin(x) + x^2 - 1;
+
 a = 0;
 b = 1;
 
-% Initialize the midpoint of the interval
 x0 = (b + a) / 2;
 
-% Loop until the desired accuracy is achieved
 while abs(b - a) > (10^(-4))
-    % Evaluate the function at the midpoint
-    f_val = f(x0);
+    f_val = subs(f, x, x0);
 
-    % Update the interval [a, b] based on the sign of f(x0)
     if f_val < 0
-        a = x0; % Update 'a' if f(x0) is negative
-    else
-        b = x0; % Update 'b' if f(x0) is positive
+        a = x0;
+    elseif f_val > 0
+        b = x0;
     end
 
-    % Update the midpoint of the interval
     x0 = (b + a) / 2;
 end
 
-% Print the final result
-fprintf("Required root is %f\n", x0);
+fprintf("Required root is %f", x0)
