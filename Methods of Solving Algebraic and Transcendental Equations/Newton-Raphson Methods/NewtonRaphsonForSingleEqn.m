c54660370fbcfbef0f59d58c9320b0dca1fc1bbc
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-10 18:51:14 +0530

    Update NewtonRaphsonForSingleEqn.m

--- Methods of Solving Algebraic and Transcendental Equations/Newton-Raphson Methods/NewtonRaphsonForSingleEqn.m
+++ Methods of Solving Algebraic and Transcendental Equations/Newton-Raphson Methods/NewtonRaphsonForSingleEqn.m
@@ -1,19 +1,25 @@
-syms x 'real'
+% Define the function f(x) as an anonymous function
+f = @(x) x.^x + x - 4;
 
-f = x^x + x - 4;
+% Define the derivative of f(x)
+df = @(x) x.^x .* (log(x) + 1) + 1;
 
+% Initial guess
 x0 = 1.4;
 
-f_val = subs(f, x, x0);
-diff_f = subs(diff(f, x), x, x0);
-x1 = x0 - (f_val / diff_f);
-
-while abs(x1 - x0) > (10^(-6))
-    x0 = x1;
+% Newton-Raphson iteration
+while true
+    f_val = f(x0);
+    diff_f = df(x0); % Exact derivative
     
-    f_val = subs(f, x, x0);
-    diff_f = subs(diff(f, x), x, x0);
     x1 = x0 - (f_val / diff_f);
+    
+    % Check for convergence
+    if abs(x1 - x0) < 1e-6
+        break;
+    end
+    
+    x0 = x1;
 end
 
-fprintf("Required root is %f", x1)
+fprintf("Root is %f \n", x1)
