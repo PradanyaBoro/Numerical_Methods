d3dbdb960b257a67ee79c161ed00b3702155c04d
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-12 00:39:13 +0530

    Update Third_Order_Milne_Simpson.m

--- Methods of Solving Algebraic and Transcendental Equations/Multi Step Methods and Predictor-Corrector Methods/Third_Order_Milne_Simpson.m
+++ Methods of Solving Algebraic and Transcendental Equations/Multi Step Methods and Predictor-Corrector Methods/Third_Order_Milne_Simpson.m
@@ -1,42 +1,51 @@
-clear
-close all
+% Define the function f(x, y) representing the differential equation
+f = @(x, y) (x - 1) * y;
 
-f = @(x, y) (x-1)*y;
-
+% Initial conditions
 x(1) = 0;
 y(1) = 1;
-h = 0.01;
-n = 2/h;
 
-y(2) = y(1) + h*f(x(1), y(1));
+% Step size
+h = 0.01;
+
+% Number of iterations
+n = 2 / h;
+
+% Perform the first iteration using forward Euler method
+y(2) = y(1) + h * f(x(1), y(1));
 x(2) = x(1) + h;
 
+% Perform the remaining iterations using the 3rd order Milne-Simpson method
 for i = 2:n
-    x(i+1) = x(i) + h;
-    y0 = y(i) + (h/2)*(3*f(x(i), y(i)) - f(x(i-1), y(i-1)));
-    f_y0 = f(x(i+1), y0);
+    x(i + 1) = x(i) + h;
+    y0 = y(i) + (h / 2) * (3 * f(x(i), y(i)) - f(x(i - 1), y(i - 1)));
+    f_y0 = f(x(i + 1), y0);
     error = 1;
     while error > 10^(-6)
-        y1 = y(i-1) + h*((1/3)*f(x(i+1), y0) + (4/3)*f(x(i), y(i)) + ...
-            (1/3)*f(x(i-1), y(i-1)));
-        f_y1 = f(x(i+1), y1);
+        y1 = y(i - 1) + h * ((1 / 3) * f(x(i + 1), y0) + (4 / 3) * f(x(i), y(i)) + ...
+            (1 / 3) * f(x(i - 1), y(i - 1)));
+        f_y1 = f(x(i + 1), y1);
         error = abs(f_y1 - f_y0);
         f_y0 = f_y1;
     end
-    y(i+1) = y1;
+    y(i + 1) = y1;
 end
 
+% Print the final solution
 fprintf("y(%s) = %f \n", num2str(x(end), '%g'), y(end));
-% Formats x(end) as a string using the most compact representation, 
-% removing trailing zeros if present, and allowing for exponential 
-% notation if necessary
 
-plot(x, y,'-ok')
+% Plot the results of the Milne-Simpson method
+plot(x, y, '-ok')
 hold on;
+
+% Solve the same differential equation using ODE45 for comparison
 xlim([0 2]);
 xspan = [0 2];
 y0 = 1;
-[x,y] = ode45(@(x, y) (x-1)*y, xspan, y0);
+[x, y] = ode45(@(x, y) (x - 1) * y, xspan, y0);
+
+% Plot the results of ODE45
 plot(x, y, 'r')
-legend({'3rd order Milne-Simpson', ...
-    'ODE45'},'Location','north')
+
+% Add legend to the plot
+legend({'Milne-Simpson Method (3rd order)', 'ODE45 Solution'}, 'Location', 'north')
