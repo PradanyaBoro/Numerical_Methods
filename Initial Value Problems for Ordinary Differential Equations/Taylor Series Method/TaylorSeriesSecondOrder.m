1616abb674dc7ac8fe6e0b871a8a53141a68bb0b
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-15 00:30:48 +0530

    Update TaylorSeriesSecondOrder.m

--- Initial Value Problems for Ordinary Differential Equations/Taylor Series Method/TaylorSeriesSecondOrder.m
+++ Initial Value Problems for Ordinary Differential Equations/Taylor Series Method/TaylorSeriesSecondOrder.m
@@ -1,23 +1,31 @@
 clear
 close all
 
+% Define the ordinary differential equation y' = f(x, y) and its derivative with respect to y
 f = @(x, y) cos(y) - 1;
-d_f = @(x, y) -sin(y)*(cos(y) - 1);
+d_f = @(x, y) -sin(y) * (cos(y) - 1);
 
+% Initial conditions
 x(1) = 0;
 y(1) = 1;
-h = 0.01;
-n = 10/h + 1;
+h = 0.01; % Step size
+n = 10 / h + 1; % Number of steps
 
-for i = 1:n
+% Main loop for the second-order Taylor series method
+for i = 1:(n - 1)
+    % Calculate x and y using the second-order Taylor series method
     x(i + 1) = x(i) + h;
-    y(i + 1) = y(i) + h*f(x(i), y(i)) + (h^2 / 2)*d_f(x(i), y(i));
+    y(i + 1) = y(i) + h * f(x(i), y(i)) + (h^2 / 2) * d_f(x(i), y(i));
 end
 
-fprintf("y(%f) = %f \n", x(n), y(n));
+% Print the final result
+fprintf("y(%f) = %f \n", x(end), y(end));
 
+% Plot the solution obtained from the second-order Taylor series method
 plot(x, y, '-oy', 'LineWidth', 2);
 hold on;
+
+% Plot the solution obtained from ode45 for comparison
 xspan = [0 10];
 y0 = 1;
 [x, y] = ode45(@(x, y) cos(y) - 1, xspan, y0);
@@ -26,4 +34,5 @@ xlim([0 10])
 xlabel('x-axis');
 ylabel('y-axis');
 
-legend({'Taylor series', 'ODE45'})
+% Change legend
+legend({'Second-order Taylor series', 'ODE45'})
