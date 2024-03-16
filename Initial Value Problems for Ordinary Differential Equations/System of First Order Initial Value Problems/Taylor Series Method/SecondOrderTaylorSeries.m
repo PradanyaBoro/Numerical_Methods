c875c34db093d243ee02fd1cbd2712c533f4014d
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-17 00:43:07 +0530

    Update SecondOrderTaylorSeries.m

--- Initial Value Problems for Ordinary Differential Equations/System of First Order Initial Value Problems/Taylor Series Method/SecondOrderTaylorSeries.m
+++ Initial Value Problems for Ordinary Differential Equations/System of First Order Initial Value Problems/Taylor Series Method/SecondOrderTaylorSeries.m
@@ -1,38 +1,45 @@
+% Clear workspace and close all figures
 clear
 close all
 
-f = @(t, y, x) - x - 1 - 0.2*y;
+% Define the system of first-order ordinary differential equations
+f = @(t, y, x) -x - 1 - 0.2 * y;
 g = @(t, y, x) y;
-df = @(t, y, x) (- 0.2)*(- x - 1 - 0.2*y) - y;
-dg = @(t, y, x) - x - 1 - 0.2*y;
 
+% Define the derivatives of the system of equations
+df = @(t, y, x) (-0.2) * (-x - 1 - 0.2 * y) - y;
+dg = @(t, y, x) -x - 1 - 0.2 * y;
+
+% Initial conditions and parameters
 x(1) = 0;
 y(1) = 0;
 t(1) = 0;
-h = 0.01;
-T = 20;
-n = T/h + 1;
+h = 0.01; % Step size
+T = 20; % Total time
+n = T / h + 1; % Number of steps
 
+% Main loop for the second-order Taylor series method
 for i = 1:(n - 1)
-    y(i + 1) = y(i) + h*f(t(i), y(i), x(i)) + (h^2 / 2)*df(t(i), y(i), x(i));
-    x(i + 1) = x(i) + h*g(t(i), y(i), x(i)) + (h^2 / 2)*dg(t(i), y(i), x(i));
+    % Compute next values using the second-order Taylor series method
+    y(i + 1) = y(i) + h * f(t(i), y(i), x(i)) + (h^2 / 2) * df(t(i), y(i), x(i));
+    x(i + 1) = x(i) + h * g(t(i), y(i), x(i)) + (h^2 / 2) * dg(t(i), y(i), x(i));
 
+    % Update time
     t(i + 1) = t(i) + h;
 end
 
-
+% Print the final result
 fprintf("x(%f) = %f \n y(%f) = %f \n", t(end), x(end), t(end), y(end));
 
+% Plot the solutions
 plot(t, x,'-ok', 'LineWidth', 2)
 hold on;
 plot(t, y, '-or', 'LineWidth', 2)
-hold on
-%xlim([0 20]);
 xlabel('t')
 title('Second Order Taylor Series Method');
 tspan = [0 T];
 Y0 = [0; 0];
-[t, Y] = ode45(@(t, Y) [- Y(2) - 1 - 0.2*Y(1); Y(1)], tspan, Y0);
+[t, Y] = ode45(@(t, Y) [-Y(2) - 1 - 0.2 * Y(1); Y(1)], tspan, Y0);
 
 y = Y(:, 1);
 x = Y(:, 2);
@@ -41,4 +48,5 @@ plot(t, y, 'b', 'LineWidth', 2)
 hold on
 plot(t, x, 'g', 'LineWidth', 2)
 
-legend({'x', 'y', 'ode x', 'ode y'}, 'location', 'southeast');
+% Change legend to a more suitable one
+legend({'x (Taylor)', 'y (Taylor)', 'x (ODE45)', 'y (ODE45)'}, 'Location', 'southeast');
