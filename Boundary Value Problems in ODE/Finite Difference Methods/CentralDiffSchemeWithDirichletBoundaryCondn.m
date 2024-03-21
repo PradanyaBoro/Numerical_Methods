135b6dd9e4df795e1a69e9037cc5b93586321f25
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-22 00:29:21 +0530

    Update central difference scheme with Dirichlet boundary condition

--- Boundary Value Problems in ODE/Finite Difference Methods/CentralDiffSchemeWithDirichletBoundaryCondn.m
+++ Boundary Value Problems in ODE/Finite Difference Methods/CentralDiffSchemeWithDirichletBoundaryCondn.m
@@ -1,30 +1,30 @@
 clear
 close all
 
+% Define the boundary conditions and interval
 a = 0;
 b = 1;
 y_a = 0;
 y_b = 1;
+
+% Step size and number of intervals
 h = 0.01;
 n = (b - a) / h + 1;
 
-%y = sym('y', [1, n - 1]);
-% eqns = [-(2 - h ^ 2) * y(1) + (1 + (h / 2) * 2) * y(2) == (h ^ 2) * x(1), 
-%     (1 - (h / 2) * 2) * y(1:end - 2) - (2 - h ^ 2) * y(2:end - 1) + (1 + (h / 2) * 2) * y(3:end) == ];
-% 
-% [A, B] = equationsToMatrix(eqns);
-% y_sol = linsolve([A, B]);
-% 
-% y = [y_a, y_sol.', y_b];
-
+% Generate x values
 x = linspace(a, b, n);
 
+% Initialize coefficient matrix A and solution vector B
 A = zeros(n - 2, n - 2);
 B = zeros(n - 2, 1);
+
+% Construct coefficient matrix A and solution vector B
 for i = 1:(n - 2)
     for j = 1:(n - 2)
+        % Diagonal elements
         if i == j
             A(i, j) = - (2 - (h ^ 2));
+        % Off-diagonal elements
         elseif j == i + 1
             A(i, j) = 1 + h;
         elseif j == i - 1
@@ -32,6 +32,7 @@ for i = 1:(n - 2)
         end
     end
 
+    % Boundary conditions for vector B
     if i == 1
         B(i, 1) = (h ^ 2) * x(i + 1) - (1 - (h / 2) * 2) * y_a;
     elseif i == (n - 2)
@@ -41,9 +42,13 @@ for i = 1:(n - 2)
     end
 end
 
+% Solve for y using linear system of equations
 y_sol = A\B;
+
+% Concatenate boundary values with solution
 y = [y_a, y_sol.', y_b];
 
+% Plot the solution
 plot(x, y, '--b');
 xlabel('x-axis');
 ylabel('y-axis');
