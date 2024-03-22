592de71d15ab8ddda7fa421c4d37fe6cd7c3e324
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-03-23 01:06:49 +0530

    Update CentralDiffSchemeWithMixedBoundaryCondn.m

--- Boundary Value Problems in ODE/Finite Difference Methods/CentralDiffSchemeWithMixedBoundaryCondn.m
+++ Boundary Value Problems in ODE/Finite Difference Methods/CentralDiffSchemeWithMixedBoundaryCondn.m
@@ -1,17 +1,20 @@
 clear
 close all
 
-a = 0;
-b = 1;
-prime_y_a = 1;
-y_b = 1;
-h = 0.01;
-n = (b - a) / h + 1;
+a = 0; % Start of the interval
+b = 1; % End of the interval
+prime_y_a = 1; % Boundary condition is given as y0 + y0' = 1
+y_b = 1; % Second boundary condition
+h = 0.01; % Step size
+n = (b - a) / h + 1; % Number of grid points
 
-x = linspace(a, b, n);
+x = linspace(a, b, n); % Discretized grid
 
+% Initialize coefficient matrix A and right-hand side vector B
 A = zeros(n - 1, n - 1);
 B = zeros(n - 1, 1);
+
+% Populate coefficient matrix A and right-hand side vector B
 for i = 1:(n - 1)
     for j = 1:(n - 1)
         if i == 1
@@ -28,6 +31,7 @@ for i = 1:(n - 1)
         end   
     end
 
+    % Define right-hand side vector B
     if i == 1
         B(i, 1) = - (h ^ 2) + 2 * h * prime_y_a * (-1);
     elseif i == (n - 1)
@@ -37,9 +41,13 @@ for i = 1:(n - 1)
     end
 end
 
+% Solve the linear system of equations
 y_sol = A\B;
+
+% Concatenate the solution with the boundary value at b
 y = [y_sol.', y_b];
 
+% Plot the solution
 plot(x, y, '--b');
 xlabel('x-axis');
 ylabel('y-axis');
