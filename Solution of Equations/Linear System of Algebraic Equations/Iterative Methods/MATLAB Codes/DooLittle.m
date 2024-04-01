55dca3c4c5f7a4e6f9d0d93e8cfd1514acbf40c1
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-04-01 18:15:11 +0530

    Add Question-5

--- Solution of Equations/Linear System of Algebraic Equations/Iterative Methods/MATLAB Codes/DooLittle.m
+++ Solution of Equations/Linear System of Algebraic Equations/Iterative Methods/MATLAB Codes/DooLittle.m
@@ -1,40 +1,43 @@
 % Define the coefficient matrix A and the constant vector B
-A = [2, -3, 4; 1, 1, 4; 3, 4, -1];
+A = [2 -3 4; 1 1 4; 3 4 -1];
 B = [8; 15; 8];
 
-% Initialize matrices U and L for the LU decomposition
-U = zeros(size(A));
-L = zeros(size(A));
-
-% Perform the LU decomposition using the Doolittle method
+% Initializing U and L matrices
 for i = 1:length(B)
-    % Assign the first row of A to the first row of U
-    U(1, i) = A(1, i);
-    % Set the diagonal elements of L to 1
-    L(i, i) = 1;
+    U(1, i) = A(1, i);  % First row of U matrix equals first row of A matrix
+    L(i, i) = 1;        % Diagonal elements of L matrix are set to 1
 end
 
-% Compute the first column of L
+% Computing first column of L matrix
 for j = 2:length(B)
-    % Compute the first column of L using the Doolittle method
-    L(j, 1) = A(j, 1) / U(1, 1);
+    L(j, 1) = A(j, 1) / U(1, 1);  % Formula for computing first column of L matrix
 end
 
-% Compute the rest of the matrices U and L
+% LU decomposition
 for i = 2:length(B)
     for j = 2:i
-        % Compute the elements of U using the Doolittle method
-        U(j, i) = A(j, i) - L(j, 1:j - 1) * U(1:j - 1, i);
+        % Adjusted formula for U(j, i) based on iteration indices
+        if i == 2 && j == 2
+            U(j, i) = A(j, i) - L(j, 1) * U(1, i);
+        elseif i == 3 && j == 2
+            U(j, i) = A(j, i) - L(j, 1) * U(1, i) - L(j, 2) * U(2, i);
+        else
+            U(j, i) = A(j, i) - L(j, 1:j - 1) * U(1:j - 1, i); 
+        end
     end
 
     for j = i + 1:length(B)
-        % Compute the elements of L using the Doolittle method
-        L(j, i) = (A(j, i) - L(j, 1:i - 1) * U(1:i - 1, i)) / U(i, i);
+        % Adjusted formula for L(j, i) based on iteration indices
+        if i == 2
+            L(j, i) = (A(j, i) - L(j, 1) * U(1, i)) / U(i, i);
+        else
+            L(j, i) = (A(j, i) - L(j, 1:i - 1) * U(1:i - 1, i)) / U(i, i);
+        end
     end
 end
 
-% Solve the system of equations using LU decomposition
-sol = inv(U) * inv(L) * B;
+% Solving the system of equations
+sol = U^(-1)*L^(-1)*B;
 
-% Print the solution
-fprintf('Required solution is (%f, %f, %f) \n', sol);
+% Displaying the solution
+fprintf('Required solution is (%f,%f,%f) \n', sol)
