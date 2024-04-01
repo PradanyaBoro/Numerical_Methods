2332855df927b60d6bf06741f4f4931be3e519ef
Pradanya Boro <152534441+PradanyaBoro@users.noreply.github.com>
2024-04-01 18:47:16 +0530

    Create DooLittle

--- Solution of Equations/Linear System of Algebraic Equations/Iterative Methods/MATLAB Codes/DooLittle.m
+++ Solution of Equations/Linear System of Algebraic Equations/Iterative Methods/MATLAB Codes/DooLittle.m
@@ -4,35 +4,25 @@ B = [8; 15; 8];
 
 % Initializing U and L matrices
 for i = 1:length(B)
-    U(1, i) = A(1, i);  % First row of U matrix equals first row of A matrix
-    L(i, i) = 1;        % Diagonal elements of L matrix are set to 1
+    U(1, i) = A(1, i);  % Setting the first row of U matrix equal to the first row of A matrix
+    L(i, i) = 1;        % Setting the diagonal elements of L matrix to 1
 end
 
-% Computing first column of L matrix
+% Computing the first column of L matrix
 for j = 2:length(B)
-    L(j, 1) = A(j, 1) / U(1, 1);  % Formula for computing first column of L matrix
+    L(j, 1) = A(j, 1) / U(1, 1);  % Computing the first column of L matrix using the formula
 end
 
 % LU decomposition
 for i = 2:length(B)
     for j = 2:i
-        % Adjusted formula for U(j, i) based on iteration indices
-        if i == 2 && j == 2
-            U(j, i) = A(j, i) - L(j, 1) * U(1, i);
-        elseif i == 3 && j == 2
-            U(j, i) = A(j, i) - L(j, 1) * U(1, i) - L(j, 2) * U(2, i);
-        else
-            U(j, i) = A(j, i) - L(j, 1:j - 1) * U(1:j - 1, i); 
-        end
+        % Computing elements of U matrix using the formula
+        U(j, i) = A(j, i) - L(j, 1:j - 1) * U(1:j - 1, i);
     end
 
     for j = i + 1:length(B)
-        % Adjusted formula for L(j, i) based on iteration indices
-        if i == 2
-            L(j, i) = (A(j, i) - L(j, 1) * U(1, i)) / U(i, i);
-        else
-            L(j, i) = (A(j, i) - L(j, 1:i - 1) * U(1:i - 1, i)) / U(i, i);
-        end
+        % Computing elements of L matrix using the formula
+        L(j, i) = (A(j, i) - L(j, 1:i - 1) * U(1:i - 1, i)) / U(i, i);
     end
 end
 
